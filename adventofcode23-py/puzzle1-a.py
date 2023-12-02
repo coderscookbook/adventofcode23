4c00e0f5497a6d8b70894dac39ba1466b04b7447
Jose A. <soigne@Aria.local>
2023-12-02 17:42:11 -0600

    WIP: p1-a.py

--- adventofcode23-py/puzzle1-a.py
+++ adventofcode23-py/puzzle1-a.py
@@ -5,8 +5,13 @@ answer = 0
 file_input_path = '../puzzle-inputs/puzzle1-input.txt'
 
 # open input file
+with open(file_input_path, 'r') as file:
 # for each line in the file:
+    count = 1
+    for line in file:
     # get first digit
+        print(count, line.strip())
+        count = count + 1
     # get last digit 
     # concatenate the digits to create new number
     # add new number to answer
