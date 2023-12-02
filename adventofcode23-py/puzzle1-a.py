17e1d111818ed81698e1621e1057820ad99c1c88
Jose A. <soigne@Aria.local>
2023-12-02 16:46:50 -0600

    Write pseudocode for p1a-py

---
+++ adventofcode23-py/puzzle1-a.py
@@ -0,0 +1,13 @@
+# import modules
+
+# create variables
+answer = 0
+file_input_path = '../puzzle-inputs/puzzle1-input.txt'
+
+# open input file
+# for each line in the file:
+    # get first digit
+    # get last digit 
+    # concatenate the digits to create new number
+    # add new number to answer
+
